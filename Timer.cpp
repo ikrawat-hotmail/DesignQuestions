6370e7e840502fe0d5dbdba06811c50c5d631087
ikrawat-hotmail <49373920+ikrawat-hotmail@users.noreply.github.com>
2025-09-06 01:15:36 +0530

    Update Timer.cpp

--- Timer.cpp
+++ Timer.cpp
@@ -1,4 +1,12 @@
-// running as a service in some code
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <functional>
+#include <queue>
+#include <condition_variable>
+#include <mutex>
+#include <future>
+
 
 class ITimer {
 public:
@@ -8,70 +16,92 @@ public:
 	virtual void cancelTimer(int timerId) = 0;
 };
 
+class TimerData {
+public:
+    std::chrono::steady_clock::time_point mExpiresAt;
+    std::function<void()> mCallback;
+    int mTimerId;
+    
+    TimerData(std::chrono::steady_clock::time_point expiresAt,
+        std::function<void()> callback,
+        int timerId
+    ) : mCallback(callback), mExpiresAt(expiresAt), mTimerId(timerId) {};
+   
+};
+
+struct TimerCompare {
+    bool operator () (const TimerData& t1, const TimerData& t2) {
+        return t1.mExpiresAt < t2.mExpiresAt; // max-heap behavior
+    }
+};
+
 
 class TimerImpl : public ITimer {
-public:	
-	ITimer* getInstance() {
-		if (timer == nullptr)
-			timer = new TimerImpl;
-		return timer;
-	}
-	
-	int createTimer(std::chrono::steady_clock::time_point expiresAt, std::function<void()> callback) {
-		TimerData *timerDataObj = new TimerData(expiresAt, callback, uniqueTimerId);
-		timerDataObj.insert(timerDataObj);
-		int uniqueTimerId += 1;
-	}
-	
-	void cancelTimer(int timerId) {
-	
-	}
-	
 private:
-	static ITimer *timer;
+	static ITimer *mTimerInstance;
 	
 	// delete copy operators
-	ITimer(const ITimer&) = delete;
-	ITimer operator=(const ITimer&) = delete;
+	TimerImpl(const TimerImpl&) = delete;
+	TimerImpl operator=(const TimerImpl&) = delete;
 	
-	ITimer() : timer {
-		cout << "Initializing a timer class" << endl;
-		std::thread t1;
-	}
-	
-	struct TimerData {
-		std::chrono::steady_clock::time_point mExpiresAt;
-		std::function<void()> mCallback;
-		int mTimerId;
-		
-		TimerData(std::chrono::steady_clock::time_point expiresAt,
-			std::function<void()> callback,
-			timerId
-		) : mCallback(callback), mExpiresAt(expiresAt), mTimerId(timerId);
-		
-		
-		bool operator()(const TimerData& t1, const TimerData& t2) {
-			return t1.mExpiresAt < t2.mExpiresAt; // max-heap behavior
-		}
-	}
+	TimerImpl() {
+		std::cout << "Initializing a timer class" << std::endl;
+        mThread = std::thread(&TimerImpl::run, this);
+	}	
 	
 	void run() {
+        std::vector<std::future<void>> waitingList;
+        std::unique_lock<std::mutex> lock(mQueueMutex);
+
+        while (true) {
+            while (mTimerQueue.size()) {
+                TimerData timerData = mTimerQueue.top();
+                auto now = std::chrono::steady_clock::now();
+                mTimerQueue.pop();
+
+                if (timerData.mExpiresAt > now) {
+                    std::future<void> result = std::async(std::launch::async, timerData.mCallback);
+                    waitingList.push_back(result);
+                } else {
+                    std::this_thread::sleep_for(timerData.mExpiresAt - now);
+                }
+            }
+            mIdleWait.wait(lock);
+        }
 	}
 	
 	void updateTimers() {
 		
 	}
 	
-	std::priority_queue<TimerData, std::vector<TimerData>, > timerExpiryQueue;
-	int uniqueTimerId = 0;
+public:	
+	static ITimer* getInstance() {
+		if (mTimerInstance == nullptr)
+			mTimerInstance = new TimerImpl;
+		return mTimerInstance;
+	}
+	
+	int createTimer(std::chrono::steady_clock::time_point expiresAt, std::function<void()> callback) {
+		TimerData timerDataObj(expiresAt, callback, mUniqueTimerId);
+		mTimerQueue.push(timerDataObj);
+        if (mTimerQueue.size() == 1)
+            mIdleWait.notify_one();
+		mUniqueTimerId += 1;
+
+        return mUniqueTimerId;
+	}
+	
+	void cancelTimer(int timerId) {
+	
+	}
+	
+
+	std::priority_queue<TimerData&, std::vector<TimerData&>, TimerCompare> mTimerQueue;
+	int mUniqueTimerId = 0;
+    std::thread mThread;
+    std::condition_variable mIdleWait;
+    std::mutex mQueueMutex;
 };
 
 
-static ITimer *timer = nullptr;
-
-
-
-// Key is
-// 1. One timer service, hence use singleton pattern
-// 2. Timer service can be implemeted using a map or priority_queue
-// 3. Running a separate thread for sleeping the closest expiry timer
+static ITimer *mTimerInstance = nullptr;
