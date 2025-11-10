8889c3384102c78cd38a83135a5467ee23339683
ishank <ishank.rawat@hotmail.com>
2025-11-10 09:18:52 +0530

    Something

--- JobScheduler/BaseJobScheduler.cpp
+++ JobScheduler/BaseJobScheduler.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <unordered_map>
 #include <map>
 #include <memory>
+#include <initializer_list>
+#include <set>
 
 #define pid_t int
+#define tid_t int
+
+using Callback = std::function<void ()>;
 
 enum STATE {
     DESTROYED,
-    BLOCKED,
+    READY,
     RUNNABLE,
     RUNNING
 };
@@ -14,10 +20,73 @@ enum STATE {
 struct JobDescriptor {
     pid_t PID;
     STATE state;
+    int priority;
+    int deadline;                                           // if used in Earliest deadline sched
+    std::unordered_map<std::string, Callback> callbacks;
 };
+
+
+/* 
+    Lifecycle of my threads
+
+    READY   -->   RUNNABLE  --> RUNNING ---> DESTROYED
+                     /|\          |
+                      |           |
+                      ------------
+*/
 
 class BaseJobScheduler {
 public:
     std::map<pid_t, std::shared_ptr<JobDescriptor>> mRunQueue;
-    virtual int registerProcess(pid PID, std::)
+
+    virtual int registerProcess(pid_t PID, std::initializer_list<std::pair<std::string, Callback>> list) {
+        std::set<std::string> requiredCallbacks = {"execute", "interrupt"};
+
+        // mandatory callback checks
+        std::set<std::string> provided;
+        for (const auto& pair : list) {
+            provided.insert(pair.first);
+        }
+
+        for (const auto& requiredcb : requiredCallbacks) {
+            if (provided.find(requiredcb) == provided.end()) {
+                std::cerr << "Required callback "<< requiredcb << "not registered.\n";
+                return -1;
+            }
+        }
+
+        auto it = mRunQueue.find(PID);
+        if (it == mRunQueue.end()) {
+            auto job = std::make_shared<JobDescriptor>();
+            job->PID = PID;
+            job->state = RUNNABLE;
+            mRunQueue[PID] = job;
+            it = mRunQueue.find(PID);
+        }
+
+        // Register callbacks
+        auto& job = it->second;
+        for (const auto& pair : list) {
+            job->callbacks[pair.first] = pair.second;
+        }
+
+        return 0;
+    }
+
+private:
+    virtual int schedule() {
+
+    }
+
+    virtual int execute(pid_t pid) {
+
+    }
+
+    virtual int interrupt(pid_t pid) {
+
+    }
+
+    virtual int destroy(pid_t pid) {
+
+    }
 };
